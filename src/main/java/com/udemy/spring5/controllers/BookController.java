1c63425c002791934993ba221b475d9c1c40ba35
Harry Wells <harry.wells@insight.com>
2019-08-07 13:06:03 -0400

    SF5.2.22 - Configuring Spring MVC Controllers

---
+++ src/main/java/com/udemy/spring5/controllers/BookController.java
@@ -0,0 +1,31 @@
+package com.udemy.spring5.controllers;
+
+import com.udemy.spring5.repositories.BookRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by hwells on 2019-08-07
+ * Package: com.udemy.spring5.controllers
+ * Description:
+ */
+
+@RestController
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+
+    }
+}
